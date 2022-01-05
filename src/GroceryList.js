1250918b962f490fb52f8eaf1c5f48fe4cd1afc4
Fateeva Iryna <fateevaira@ukr.net>
2022-01-05 09:31:02 +0200

    first commit

---
+++ src/GroceryList.js
@@ -0,0 +1,55 @@
+import { Component } from "react";
+import check from './ok.jpg'
+
+export class GroceryList extends Component {
+    state = {
+        userInput: "",
+        groceryList: []
+    }
+
+    onChangeEvent(e) {
+        this.setState ({userInput: e})
+    }
+
+    addItem(input){
+        if(input==='') {
+            alert('Please enter an item')
+        } else {
+        let listArray = this.state.groceryList;
+        listArray.push(input);
+        this.setState({groceryList: listArray, userInput:''});
+        }
+    }
+    deleteItem(){
+        let listArray = this.state.groceryList;
+        listArray = [];
+        this.setState({groceryList: listArray})
+    }
+    crossedWord(event) {
+        const li = event.target;
+        li.classList.toggle('crossed');
+    }
+     render() {
+         return (
+             <div>
+                 <div className="container">
+                    <input type="text"
+                    placeholder="What to you want to by today?"
+                    onChange = {(e) => {this.onChangeEvent(e.target.value)}}
+                    value={this.state.userInput}/>
+                 </div>
+                 <div className="container">
+                    <button onClick={() => this.addItem(this.state.userInput)} className="btn add">Add</button>
+                 </div>
+                 <ul>
+                     {this.state.groceryList.map((item, index) => (
+                         <li onClick={this.crossedWord} key={ index }> <img src={ check } width='40px' alt="check"/> { item }</li>
+                     ))}
+                 </ul>
+                 <div className="container">
+                    <button onClick={() => this.deleteItem()} className="btn delete">Delete</button>
+                 </div>
+             </div>
+         )
+     }
+}
